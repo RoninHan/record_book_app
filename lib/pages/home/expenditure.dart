1e29b85c06786c243e66962764cc57d3668fa86a
Ronin <xunyingshadow@gmail.com>
2023-04-23 17:32:50 +0800

    test

--- lib/pages/home/expenditure.dart
+++ lib/pages/home/expenditure.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:record_book_app/widgets/button-grid.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../utils/utils.dart';
 
 class Expenditure extends StatefulWidget {
   const Expenditure({Key? key}) : super(key: key);
@@ -14,33 +17,38 @@ class _ExpenditureState extends State<Expenditure> {
   Color bgColor = Colors.amber;
 
   static List<GridItem> gridItem = [
-    GridItem(menoyType: "餐饮", color: Colors.amber, icon: Icons.local_dining),
     GridItem(
-        menoyType: "零食",
-        color: Color.fromARGB(255, 215, 85, 238),
-        icon: Icons.fastfood),
-    GridItem(
-        menoyType: "烟酒饮料",
-        color: Color.fromARGB(255, 62, 161, 243),
-        icon: Icons.local_drink),
-    GridItem(
-        menoyType: "购物",
-        color: Color.fromARGB(255, 246, 92, 92),
-        icon: Icons.shopping_cart),
-    GridItem(
-        menoyType: "交通", color: Colors.greenAccent, icon: Icons.directions_bus),
-    GridItem(
-        menoyType: "话费",
-        color: Color.fromARGB(255, 46, 153, 247),
-        icon: Icons.phone_android),
-    GridItem(
-        menoyType: "居住",
-        color: const Color.fromRGBO(231, 140, 104, 1.0),
-        icon: Icons.home_outlined),
-    GridItem(
-        menoyType: "水电",
-        color: Color.fromARGB(255, 220, 132, 97),
-        icon: Icons.energy_savings_leaf_outlined),
+        menoyType: "餐饮",
+        color: Utils.getColorFromString(""),
+        icon: IconData(0xf858,
+            fontFamily: 'FontAwesomeSolid',
+            fontPackage: 'font_awesome_flutter')),
+    // GridItem(
+    //     menoyType: "零食",
+    //     color: Color.fromARGB(255, 215, 85, 238),
+    //     icon: IconData(0xf858, fontFamily: 'FontAwesomeSolid', fontPackage: 'font_awesome_flutter')),
+    // GridItem(
+    //     menoyType: "烟酒饮料",
+    //     color: Color.fromARGB(255, 62, 161, 243),
+    //     icon: Icons.local_drink),
+    // GridItem(
+    //     menoyType: "购物",
+    //     color: Color.fromARGB(255, 246, 92, 92),
+    //     icon: Icons.shopping_cart),
+    // GridItem(
+    //     menoyType: "交通", color: Colors.greenAccent, icon: Icons.directions_bus),
+    // GridItem(
+    //     menoyType: "话费",
+    //     color: Color.fromARGB(255, 46, 153, 247),
+    //     icon: Icons.phone_android),
+    // GridItem(
+    //     menoyType: "居住",
+    //     color: const Color.fromRGBO(231, 140, 104, 1.0),
+    //     icon: Icons.home_outlined),
+    // GridItem(
+    //     menoyType: "水电",
+    //     color: Color.fromARGB(255, 220, 132, 97),
+    //     icon: Icons.energy_savings_leaf_outlined),
   ];
 
   final TextEditingController _menoyController = TextEditingController();
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
 
 class Utils {
   ///使用md5加密
@@ -9,5 +10,11 @@ class Utils {
     Uint8List content = new Utf8Encoder().convert(data);
     Digest digest = md5.convert(content);
     return digest.toString();
+  }
+
+  static Color getColorFromString(String str) {
+    String cleanStr = str.replaceAll('#', '');
+    int colorInt = int.parse(cleanStr, radix: 16);
+    return Color(colorInt).withOpacity(1.0);
   }
 }
